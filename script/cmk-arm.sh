e7af8df5f10d2bb2706739b95cc467d8cb20baf2
HuYapeng <hyptender@gmail.com>
2026-08-11 18:08:11 +0800

    build(examples): 将项目事件例程加入多架构构建目标

--- script/cmk-arm.sh
+++ script/cmk-arm.sh
@@ -30,6 +30,7 @@ cmake --build "${BUILD_DIR}" --target \
     l4_minidb_config \
     l4_uart_config \
     l4_socket_transfer \
+    l4_project_event \
     l4_daemon \
     -j${num}
 cmake --install "${BUILD_DIR}"
